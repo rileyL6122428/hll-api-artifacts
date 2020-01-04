afe797ec1fd28de7bfe4127e78fd3f6db242b480
Riley Littlefield <rileylittlefield@ymail.com>
2020-01-04 12:51:22 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,8 +8,8 @@ mv guava-26.0-jre.jar server-executables/libs/guava-26.0-jre.jar
 curl -o hll-api-0.0.1-SNAPSHOT.jar https://raw.githubusercontent.com/rileyL6122428/hll-api-artifacts/master/hll-api-0.0.1-SNAPSHOT.jar
 mv hll-api-0.0.1-SNAPSHOT.jar server-executables/hll-api-0.0.1-SNAPSHOT.jar
 
-curl -o ffprobe https://raw.githubusercontent.com/rileyL6122428/hll-api-artifacts/master/ffprobe
-mv ffprobe server-executables/ffprobe
+# curl -o ffprobe https://raw.githubusercontent.com/rileyL6122428/hll-api-artifacts/master/ffprobe
+# mv ffprobe server-executables/ffprobe
 
 sudo yum install java-1.8.0-openjdk-devel
 cd server-executables
